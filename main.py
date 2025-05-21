3b723a7a70a2c9787819d0e2dfc7dd72ea798e02
Luciano Nicolas Viscio Frank <140804627+NicoViscio@users.noreply.github.com>
2025-05-21 17:21:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from model.user_model import UserModel
 from view.login_view import LoginView
 from controller.controller import Controller
 
-# Punto de entrada principal de la aplicación. Se inicializa la aplicación Qt,
 # se crean las instancias del modelo, la vista y el controlador, y se lanza la interfaz gráfica.
 if __name__ == "__main__":
     app = QApplication(sys.argv)
